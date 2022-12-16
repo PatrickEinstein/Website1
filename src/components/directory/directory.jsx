1ee51fe274c9fe2b801662e1988d31d119f9b3c1
PatrickEinstein <mohammedola1234@gmail.com>
2022-12-16 11:38:28 +0100

    website 1

---
+++ src/components/directory/directory.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+
+import MenuItem from "../menu-items/menu-items";
+
+import "./directory.css";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          id: 1,
+          linkUrl: "shop/hats",
+        },
+
+        {
+          title: "jackets",
+          imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          id: 2,
+          linkUrl: "shop/jackets",
+        },
+
+        {
+          title: "sneakers",
+          imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          id: 3,
+          linkUrl: "shop/sneakers",
+        },
+
+        {
+          title: "womens",
+          imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          size: "large",
+          id: 4,
+          linkUrl: "shop/womens",
+        },
+
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          size: "large",
+          id: 5,
+          linkUrl: "shop/mens",
+        },
+
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/3F3YMSj/4.png",
+          size: "large",
+          id: 6,
+          linkUrl: "shop/mens",
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/3F3YMSj/4.png",
+          size: "large",
+          id: 7,
+          linkUrl: "shop/mens",
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/3F3YMSj/4.png",
+          size: "large",
+          id: 8,
+          linkUrl: "shop/mens",
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/3F3YMSj/4.png",
+          size: "large",
+          id: 9,
+          linkUrl: "shop/mens",
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/3F3YMSj/4.png",
+          size: "large",
+          id: 10,
+          linkUrl: "shop/mens",
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/3F3YMSj/4.png",
+          size: "large",
+          id: 11,
+          linkUrl: "shop/mens",
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/3F3YMSj/4.png",
+          size: "large",
+          id: 12,
+          linkUrl: "shop/mens",
+        },
+
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/3F3YMSj/4.png",
+          size: "large",
+          id: 13,
+          linkUrl: "shop/mens",
+        },
+        {
+          title: "mens",
+          imageUrl: "https://i.ibb.co/3F3YMSj/4.png",
+          size: "large",
+          id: 14,
+          linkUrl: "shop/mens",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
